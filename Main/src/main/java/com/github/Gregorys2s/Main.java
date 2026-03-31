4dbcbcd6149b340fa332fcf8575b5cf69abe4f39
Gregory <necrondragonss@gmail.com>
2026-03-31 18:28:06 -0300

    testando los drivers

--- Main/src/main/java/com/github/Gregorys2s/Main.java
+++ Main/src/main/java/com/github/Gregorys2s/Main.java
@@ -5,6 +5,13 @@ import javax.sql.*;
 
 public class Main {
     public static void main(String[] args) {
+        try (Connection c = DriverManager.getConnection("jdbc:postgresql://localhost:5432/Lanchonete", "postgres", "admin")) {
+        String db = c.getMetaData().getDatabaseProductName();
+        String ver = c.getMetaData().getDatabaseProductVersion();
 
+        System.out.println(db + "  " + ver + " Te da la bienvenida");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
