6141ff34d8ccbb2f3dacfff09bce9feefb9c6aca
Gregory <necrondragonss@gmail.com>
2026-03-31 17:48:44 -0300

    comentario dos imports

--- Main/src/main/java/com/github/Gregorys2s/Main.java
+++ Main/src/main/java/com/github/Gregorys2s/Main.java
@@ -1,18 +1,10 @@
 package com.github.Gregorys2s;
-
+//pacotes para comunicação com o bando de dados (JBDC)
 import java.sql.*;
 import javax.sql.*;
 
 public class Main {
     public static void main(String[] args) {
-    DriverManager.drivers().forEach(
-            driver -> System.out.println(driver.toString())
-    );
-
-    for (Driver d : DriverManager.drivers().toList())
-    {
-        System.out.println(d.toString());
-    }
 
     }
 }
