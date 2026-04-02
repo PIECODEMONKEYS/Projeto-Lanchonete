5fa37f55039a9676d0ac6919a2368b6e45e1da71
pedro <pedro.m.dias.d.moraes@gmail.com>
2026-04-02 16:50:19 -0300

    Fazendo o cardapio falta teste sse  a classe funciona e fazer JPAUtil e CardapioRepository (nao consegui colocar o nome do JPAUtil de entity manager)

--- beltt/src/main/java/com/github/sistema_lanchonete/repositories/JPAUtil.java
+++ beltt/src/main/java/com/github/sistema_lanchonete/repositories/JPAUtil.java
@@ -4,7 +4,7 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
-
+//era pra ser entity manager mas o intelij marcou erro dai pedi pro hemini dar outro nome(codgio do prof)
 public class JPAUtil {
 
     private static final EntityManagerFactory emf;
