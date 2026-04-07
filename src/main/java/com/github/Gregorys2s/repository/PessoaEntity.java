9b4ec8dc656ecb5805e3308e8115bb34cb10a4ab
Gregory <necrondragonss@gmail.com>
2026-04-07 11:49:18 -0300

    foi arrumado o pom.xml, foi testada a conexao com o banco de dados, e criadas as pastas para organizar

---
+++ src/main/java/com/github/Gregorys2s/repository/PessoaEntity.java
@@ -0,0 +1,10 @@
+package com.github.Gregorys2s.repository;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "ingredientes")
+
+public class PessoaEntity {
+}
