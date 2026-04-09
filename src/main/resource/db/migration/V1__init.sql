a1794b5ab1adc5bb79b56893d163a0ceef7da9b2
Gregory <necrondragonss@gmail.com>
2026-04-09 01:37:23 -0300

    foi actualizada as tabelas do db do v_1 e criado o relacionamento entre tabelas

--- src/main/resource/db/migration/V1__init.sql
+++ src/main/resource/db/migration/V1__init.sql
@@ -2,5 +2,40 @@ create table ingredientes
 (
     id serial not null primary key,
     nome varchar(30) not null,
-    estoque integer not null
-)
+    estoque integer not null,
+    constraint ingr_no_repeat_name unique (nome)
+);
+
+create table produtos(
+    id serial primary key,
+    nome varchar(100) not null,
+    preco numeric(10,2) not null
+);
+
+
+create table pedidos(
+    id serial primary key,
+      data_hora timestamp default CURRENT_TIMESTAMP null,
+      valor_total numeric(10,2) not null
+);
+
+
+create table pedidos_produtos(
+    id serial primary key,
+    fk_pedido_id int,
+    fk_produto_id int,
+    quantidade int,
+
+    foreign key (fk_produto_id) references produtos(id),
+    foreign key (fk_pedido_id) references pedidos(id)
+);
+
+
+create table ingredientes_produtos(
+    id serial primary key,
+    fk_produto_id int,
+    fk_ingredientes_id int,
+
+    foreign key (fk_produto_id) references produtos(id),
+    foreign key (fk_ingredientes_id) references ingredientes(id)
+);
