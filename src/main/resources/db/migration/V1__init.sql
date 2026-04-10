ad833e18d872bb4d1c5d9585993f0752d1a3df09
FelipeYuji <fjapa977@gmail.com>
2026-04-10 08:15:10 -0300

    Criei/fiz minha dos produtos e estoque com seus dados, tipos de dados e a comunicaÃção entre o banco de dados e o ide

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -2,5 +2,6 @@ create table ingredientes
 (
     id serial not null primary key,
     nome varchar(30) not null,
-    estoque integer not null
+    estoque integer not null,
+    frequencia varchar(20) not null
 )
