416a32e9b152234c617306f14c057a6d23643c79
Gregory <necrondragonss@gmail.com>
2026-04-10 05:15:17 -0300

    foram arrumados os bugs, foi trocado as configuracoes do hibernate, inseridos novos migrations (triggers e tabelas), e foi testado

--- beltt/target/classes/db/migration/V5__realizar_pedidos_teste.sql
+++ beltt/target/classes/db/migration/V5__realizar_pedidos_teste.sql
@@ -4,8 +4,8 @@ INSERT INTO pedidos (valor_total) VALUES (51.40); -- Pedido ID 1
 -- 2. Adicionando itens ao Pedido 1
 -- Vamos pedir 1 X-Bacon Monstro (ID 2) e 1 X-Burguer Clássico (ID 1)
 INSERT INTO pedidos_produtos (fk_pedido_id, fk_produto_id, quantidade) VALUES
-                                                                           (1, 2, 1), -- 1x X-Bacon
-                                                                           (1, 1, 1); -- 1x X-Burguer
+(1, 2, 1), -- 1x X-Bacon
+(1, 1, 1); -- 1x X-Burguer
 
 
 -- 3. Criando o Pedido do Cliente B
